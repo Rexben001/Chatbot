48b8589d7a395d73ee2923d6f0a00179e95cea3e
Rexben001 <rexben.rb@gmail.com>
2020-02-01 18:59:42 +0100

    initial commit

--- index.js
+++ index.js
@@ -16,57 +16,58 @@ app.get("/", (req, res) => {
   res.status(200).send("Server is working.");
 });
 
-app.post("/getmovie", (req, res) => {
-  //   try {
-  const movieToSearch =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.movie
-      ? req.body.result.parameters.movie
-      : "";
+app.post("/getmovie", async (req, res) => {
+  try {
+    const movieToSearch =
+      req.body.result &&
+      req.body.result.parameters &&
+      req.body.result.parameters.movie
+        ? req.body.result.parameters.movie
+        : "";
 
-  const reqUrl = encodeURI(
-    `http://www.omdbapi.com/?t=${movieToSearch}&apikey=${process.env.API_KEY}`
-  );
-  // const reqUrl = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${movieToSearch}`;
-  http.get(
-    reqUrl,
-    responseFromAPI => {
-      let completeResponse = "";
-      responseFromAPI.on("data", chunk => {
-        completeResponse += chunk;
-      });
-      responseFromAPI.on("end", () => {
-        const movie = JSON.parse(completeResponse);
+    //   const reqUrl = encodeURI(
+    //     `http://www.omdbapi.com/?t=${movieToSearch}&apikey=${process.env.API_KEY}`
+    //   );
+    const reqUrl = encodeURI(
+      `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&query=${movieToSearch}`
+    );
+    //   http.get(
+    //     reqUrl2,
+    //     responseFromAPI => {
+    //       let completeResponse = "";
+    //       responseFromAPI.on("data", chunk => {
+    //         completeResponse += chunk;
+    //       });
+    //       responseFromAPI.on("end", () => {
+    //         const movie = JSON.parse(completeResponse);
 
-        console.log(movie, completeResponse), movieToSearch;
-        let dataToSend = movieToSearch;
-        dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${movie.Director} and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.
-                    }`;
+    //         let dataToSend = movieToSearch;
+    //         dataToSend = `${movie.Title} was released in the year ${movie.Year}. It is directed by ${movie.Director} and stars ${movie.Actors}.\n Here some glimpse of the plot: ${movie.Plot}.
+    //                     }`;
 
-        return res.json({
-          fulfillmentText: dataToSend,
-          source: "getmovie"
-        });
-      });
-    },
-    error => {
-      return res.json({
-        fulfillmentText: "Could not get results at this time",
-        source: "getmovie"
-      });
-    }
-  );
-  //     const movieResult = await axios.get(reqUrl.replace(" ", "%20"));
-  //     const response = movieResult.data.results[0];
-  //     const dataToSend = `${response.title} was released in the year ${response.release_date}.`;
-  //     res.status(200).json({
-  //       fulfillmentText: dataToSend,
-  //       source: "getmovie"
-  //     });
-  //   } catch (error) {
-  //     res.status(500).json({ error });
-  //   }
+    //         return res.json({
+    //           fulfillmentText: dataToSend,
+    //           source: "getmovie"
+    //         });
+    //       });
+    //     },
+    //     error => {
+    //       return res.json({
+    //         fulfillmentText: "Could not get results at this time",
+    //         source: "getmovie"
+    //       });
+    //     }
+    // );
+    const movieResult = await axios.get(reqUrl.replace(" ", "%20"));
+    const response = movieResult.data.results[0];
+    const dataToSend = `${response.title} was released in the year ${response.release_date}.`;
+    res.status(200).json({
+      fulfillmentText: dataToSend,
+      source: "getmovie"
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 });
 
 app.listen(port, () => {
